7a544897615deaae145fd57cc59693874520d1f1
yangqing <yangqing@jiunidu.com>
2023-01-13 10:23:48 +0800

    update json

--- pages/homeLookDetails/index.js
+++ pages/homeLookDetails/index.js
@@ -4,89 +4,12 @@ const utils = require('../../utils/utils.js')
 const request = require('../../utils/request.js')
 const box = require('../../utils/box.js')
 
-// let chart = null;
-
-// function initChart(canvas, width, height, dpr) {
-//   chart = echarts.init(canvas, null, {
-//     width: width,
-//     height: height,
-//     devicePixelRatio: dpr // new
-//   });
-//   canvas.setChart(chart);
-
-//   var option = {
-//     title: {
-//       text: '',
-//       left: 'center'
-//     },
-//     legend: {
-//       data: ['A', 'B', 'C','D'],
-//       top: 20,
-//       left: 'center',
-//       backgroundColor: 'red',
-//       z: 100
-//     },
-//     grid: {
-//       containLabel: true
-//     },
-//     tooltip: {
-//       show: true,
-//       trigger: 'axis'
-//     },
-//     xAxis: {
-//       type: 'category',
-//       boundaryGap: false,
-//       data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日', '周十', '周八', '周九','周七'],
-//       // show: false
-//     },
-//     yAxis: {
-//       x: 'center',
-//       type: 'value',
-//       splitLine: {
-//         lineStyle: {
-//           type: 'dashed'
-//         }
-//       }
-//       // show: false
-//     },
-//     series: [{
-//       name: 'A',
-//       type: 'line',
-//       smooth: true,
-//       data: [18, 36, 65, 30, 78, 40, 33,23,32,41,50]
-//     }, {
-//       name: 'B',
-//       type: 'line',
-//       smooth: true,
-//       data: [12, 50, 51, 35, 70, 30, 20,23,32,41,90]
-//     }, {
-//       name: 'C',
-//       type: 'line',
-//       smooth: true,
-//       data: [10, 30, 31, 50, 40, 20, 10,23,32,41,30]
-//     },{
-//       name: 'D',
-//       type: 'line',
-//       smooth: true,
-//       data: [40, 60, 66, 50, 80, 20, 10,23,32,41,60]
-//     }]
-//   };
-
-//   chart.setOption(option);
-//   return chart;
-// }
-
-
-
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    // ec: {
-    //   onInit: initChart
-    // },
     ec_pig: {
       lazyLoad: true // 将 lazyLoad 设为 true 后，需要手动初始化图表
     },
@@ -120,134 +43,6 @@ Page({
 
     isShow: false,
 
-    // homePersonal: [{
-    //   "id": 1,
-    //   "real_name": "张1",
-    //   "company_serial": "TJhc",
-    //   "selected": false
-    // },{
-    //   "id": 2,
-    //   "real_name": "张2",
-    //   "company_serial": "TJhc",
-    //   "selected": false
-    // },{
-    //   "id": 3,
-    //   "real_name": "张三",
-    //   "company_serial": "TJhc",
-    //   "selected": false
-    // },{
-    //   "id": 4,
-    //   "real_name": "张4",
-    //   "company_serial": "TJhc",
-    //   "selected": false
-    // },{
-    //   "id": 5,
-    //   "real_name": "张5",
-    //   "company_serial": "TJhc",
-    //   "selected": false
-    // },{
-    //   "id": 6,
-    //   "real_name": "张6",
-    //   "company_serial": "TJhc",
-    //   "selected": false
-    // },{
-    //   "id": 7,
-    //   "real_name": "张7",
-    //   "company_serial": "TJhc",
-    //   "selected": false
-    // },{
-    //   "id": 8,
-    //   "real_name": "张8",
-    //   "company_serial": "TJhc",
-    //   "selected": false
-    // },{
-    //   "id": 9,
-    //   "real_name": "张9",
-    //   "company_serial": "TJhc",
-    //   "selected": false
-    // },{
-    //   "id": 10,
-    //   "real_name": "张10",
-    //   "company_serial": "TJhc",
-    //   "selected": false
-    // },{
-    //   "id": 11,
-    //   "real_name": "张11",
-    //   "company_serial": "TJhc",
-    //   "selected": false
-    // },{
-    //   "id": 12,
-    //   "real_name": "张12",
-    //   "company_serial": "TJhc",
-    //   "selected": false
-    // },{
-    //   "id": 13,
-    //   "real_name": "张13",
-    //   "company_serial": "TJhc",
-    //   "selected": false
-    // },{
-    //   "id": 14,
-    //   "real_name": "张14",
-    //   "company_serial": "TJhc",
-    //   "selected": false
-    // }],
-    // homePersonalOld: [{
-    //   "id": 1,
-    //   "real_name": "张1",
-    //   "company_serial": "TJhc",
-    // },{
-    //   "id": 2,
-    //   "real_name": "张2",
-    //   "company_serial": "TJhc",
-    // },{
-    //   "id": 3,
-    //   "real_name": "张三",
-    //   "company_serial": "TJhc",
-    // },{
-    //   "id": 4,
-    //   "real_name": "张4",
-    //   "company_serial": "TJhc",
-    // },{
-    //   "id": 5,
-    //   "real_name": "张5",
-    //   "company_serial": "TJhc",
-    // },{
-    //   "id": 6,
-    //   "real_name": "张6",
-    //   "company_serial": "TJhc",
-    // },{
-    //   "id": 7,
-    //   "real_name": "张7",
-    //   "company_serial": "TJhc",
-    // },{
-    //   "id": 8,
-    //   "real_name": "张8",
-    //   "company_serial": "TJhc",
-    // },{
-    //   "id": 9,
-    //   "real_name": "张9",
-    //   "company_serial": "TJhc",
-    // },{
-    //   "id": 10,
-    //   "real_name": "张10",
-    //   "company_serial": "TJhc",
-    // },{
-    //   "id": 11,
-    //   "real_name": "张11",
-    //   "company_serial": "TJhc",
-    // },{
-    //   "id": 12,
-    //   "real_name": "张12",
-    //   "company_serial": "TJhc",
-    // },{
-    //   "id": 13,
-    //   "real_name": "张13",
-    //   "company_serial": "TJhc",
-    // },{
-    //   "id": 14,
-    //   "real_name": "张14",
-    //   "company_serial": "TJhc",
-    // }],
     homePersonal: [],
     homePersonalOld: [],
     homePersonalIds: [],
@@ -257,22 +52,13 @@ Page({
     this.setData({
       isShow: true
     });
-    console.log('clickName homePersonalIds---->:', this.data.homePersonalIds)
-    console.log('clickName homePersonalOldIds---->:', this.data.homePersonalOldIds)
-    console.log('clickName homePersonal---->:', this.data.homePersonal)
-    console.log('clickName homePersonalOld---->:', this.data.homePersonalOld)
-
   },
   hideModal(e) {
     this.setData({
       isShow: false
     });
     if(this.data.homePersonalIds.length > 0){
-      console.log('111111')
       if(this.data.homePersonalOldIds.length > 0){
-        console.log('222222')
-        console.log('222222',this.data.homePersonalOldIds)
-        console.log('222222',this.data.homePersonalOld)
 
         let homePersonal = this.data.homePersonalOld;
         for(let i = 0; i< homePersonal.length; i++){
@@ -293,7 +79,6 @@ Page({
           homePersonal: homePersonal
         });
       } else {
-        console.log('333333',this.data.homePersonalOld)
         let homePersonal = this.data.homePersonalOld;
         for(let i = 0; i< homePersonal.length; i++){
           homePersonal[i].selected = false;
@@ -306,9 +91,6 @@ Page({
       }
     } else {
       if(this.data.homePersonalOldIds.length > 0){
-        console.log('222222')
-        console.log('222222',this.data.homePersonalOldIds)
-        console.log('222222',this.data.homePersonalOld)
 
         let homePersonal = this.data.homePersonalOld;
         for(let i = 0; i< homePersonal.length; i++){
@@ -351,7 +133,6 @@ Page({
         homePersonalOldIds: [].concat(this.data.homePersonalIds||[]),
       });
     } else {
-      // box.showToast("请选择员工姓名");
       this.setData({
         homePersonalIds: [],
         // homePersonal: this.data.homePersonalOld
@@ -360,21 +141,16 @@ Page({
 
     this.getIwadomlistinfo();
     this.getIwadomchartinfo();
-    console.log('--homePersonalOldIds-->:',this.data.homePersonalOldIds)
   },
   checkboxChange(e) {
-    console.log('checkboxChange e:', e);
     let string = "homePersonal[" + e.target.dataset.index + "].selected"
-    console.log('checkboxChange string:', string);
     this.setData({
       [string]: !this.data.homePersonal[e.target.dataset.index].selected
     })
     let detailValue = this.data.homePersonal.filter(it => it.selected).map(it => it.id)
-    console.log('所有选中的值为：', detailValue)
     this.setData({
       homePersonalIds: detailValue
     });
-    console.log('homePersonalIds---->:', this.data.homePersonalIds,this.data.homePersonal)
   },
   bindPickerChangeResult(e) {
     console.log(e.detail.value)
@@ -384,7 +160,6 @@ Page({
       status: this.data.resultListId[e.detail.value],
       page: 1
     });
-    console.log(this.data.status)
 
     this.getIwadomlistinfo();
     this.getIwadomchartinfo();
@@ -397,7 +172,6 @@ Page({
       workType: this.data.workListId[e.detail.value],
       page: 1
     });
-    console.log(this.data.workType)
 
     this.getIwadomlistinfo();
     this.getIwadomchartinfo();
@@ -484,7 +258,6 @@ Page({
   formatYear(datestring, typestring, datetypestring) {
     if (datestring) {
       let dateList = datestring.split('-');
-      console.log('---->:', dateList)
       let start_time = dateList[0] + "年" + dateList[1] + "月" + dateList[2] + "日";
       this.setData({
         [typestring]: start_time,
@@ -497,14 +270,12 @@ Page({
     }
   },
   bindStartTimeChange: function (e) {
-    console.log('picker发送选择改变，携带值为', e.detail.value)
     let datestring = e.detail.value;
     if (datestring) {
       this.formatYear(datestring, 'start_time', 'startDate');
     }
   },
   bindEndTimeChange: function (e) {
-    console.log('picker发送选择改变，携带值为', e.detail.value)
     let datestring = e.detail.value;
     if (datestring) {
       this.formatYear(datestring, 'end_time', 'endDate');
@@ -608,27 +379,6 @@ Page({
       //     y:'dataMin'
       // },
       series: series
-      // series: [{
-      //   name: 'A',
-      //   type: 'line',
-      //   smooth: true,
-      //   data: [18, 36, 65, 30, 78, 40, 33, 23, 32, 41, 50, 90, 90, 90, 90, 90]
-      // }, {
-      //   name: 'B',
-      //   type: 'line',
-      //   smooth: true,
-      //   data: [12, 50, 51, 35, 70, 30, 20, 23, 32, 41, 90, 90, 90, 90, 90, 90]
-      // }, {
-      //   name: 'C',
-      //   type: 'line',
-      //   smooth: true,
-      //   data: [10, 30, 31, 50, 40, 20, 10, 23, 32, 41, 30, 90, 90, 90, 90, 90]
-      // }, {
-      //   name: 'D',
-      //   type: 'line',
-      //   smooth: true,
-      //   data: [40, 60, 66, 50, 80, 20, 10, 23, 32, 41, 60, 90, 90, 90, 90, 90]
-      // }]
     };
     chart.setOption(option);
     return chart;
@@ -674,7 +424,6 @@ Page({
       if (res) {
         if (res.success) {
           var xdata = res.xdata
-          // var ydata = ['10', '30', '60', '90', '20', '70', '80', '24', '55', '67', '40']
           var ydata = []
           var series = res.ydata
           var axisLabel = { //设置x轴的字
@@ -683,14 +432,6 @@ Page({
           }
           that.chartInit(xdata, ydata, axisLabel, series)
 
-        //   var xdata = ['张一','张一','张一','张一','张一','张一','张一','张一','张一','张一','张二','张三','张4','张5','张6','张7']
-        // // var ydata = ['10','30','60','90','20','70','80','24','55','67','40']
-        // var ydata = []
-        // var axisLabel = { //设置x轴的字
-        //   show:true,
-        //   interval:0,
-      // }
-    // that.chartInit(xdata,ydata,axisLabel)
         } else {
           box.showToast(res.msg);
         }
